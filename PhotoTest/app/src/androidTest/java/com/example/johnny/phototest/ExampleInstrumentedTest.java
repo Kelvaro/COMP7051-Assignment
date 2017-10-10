a8ef56321b0abd5c1d395e48becf80e00c9e406a
Kelvaro <blizzardkang@gmail.com>
2017-10-09 19:23:52 -0700

    Added Tests that performs typing in input and clicking button

--- PhotoTest/app/src/androidTest/java/com/example/johnny/phototest/ExampleInstrumentedTest.java
+++ PhotoTest/app/src/androidTest/java/com/example/johnny/phototest/ExampleInstrumentedTest.java
@@ -1,12 +1,21 @@
 package com.example.johnny.phototest;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +25,37 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.johnny.phototest", appContext.getPackageName());
+
+
+
+
+
+
     }
+
+    @Test
+    public void Checkdate() {
+
+        onView(withId(R.id.editText)).perform(typeText("20160202")), closeSoftKeyboard();
+
+        onView(withId(R.id.editText2)).perform(typeText("20120606")), closeSoftKeyboard();
+
+
+        onView(withId(R.id.button)).perform(click());
+    }
+
+
+
 }
