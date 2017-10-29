90a5131621282c3098d5290387c5b7493f21adde
Kelvaro <blizzardkang@gmail.com>
2017-10-28 22:40:24 -0700

    test

--- PhotoTest/app/src/main/java/com/example/johnny/phototest/MainActivity.java
+++ PhotoTest/app/src/main/java/com/example/johnny/phototest/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity{
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-
+//testing
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
                 Log.d("Files", "Permission Exists");
             else
