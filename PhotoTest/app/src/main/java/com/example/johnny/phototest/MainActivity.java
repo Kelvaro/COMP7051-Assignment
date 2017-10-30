be80a3e0eb8f3d80c67e1a981cf51d3e687f6698
Kelvaro <blizzardkang@gmail.com>
2017-10-29 19:43:39 -0700

    Attempting to fix array null

--- PhotoTest/app/src/main/java/com/example/johnny/phototest/MainActivity.java
+++ PhotoTest/app/src/main/java/com/example/johnny/phototest/MainActivity.java
@@ -54,11 +54,11 @@ public class MainActivity extends AppCompatActivity{
             //List Directory and note tempfile.txt
             File directory = new File(path);
             File[] files = directory.listFiles();
-            Log.d("Files", "Size: "+ files.length);
-            for (int i = 0; i < files.length; i++)
-            {
-                Log.d("Files", "FileName:" + files[i].getName());
-            }
+            //Log.d("Files", "Size: "+ files.length);
+           // for (int i = 0; i < files.length; i++)
+          //  {
+             //   Log.d("Files", "FileName:" + files[i].getName());
+          //  }
 
 
 
